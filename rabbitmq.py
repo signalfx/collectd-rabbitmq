c28a2150b2e02c762b46e776fdf60105bad11740
Charlie Smith <charlie@signalfx.com>
2017-06-16 13:15:08 -0400

    fix python2.6 incompatible syntax (#12)
    * fix python2.6 incompatible syntax

--- rabbitmq.py
+++ rabbitmq.py
@@ -250,7 +250,7 @@ def config(config_values):
     b = Broker()
 
     # We use only single values so this works fine
-    config_map = {c.key: c.values[0] for c in config_values.children}
+    config_map = dict([(c.key, c.values[0]) for c in config_values.children])
 
     required_keys = ('Username', 'Password', 'Host', 'Port')
 
