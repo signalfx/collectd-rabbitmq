22a631a6b5dc41d36b5042f8cfd987d9d309f89a
Charlie Smith <charlie@signalfx.com>
2017-06-16 16:57:23 -0400

    add integration tests against amazon 2017.03

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -4,9 +4,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-docker-compose run --rm -T test
+printf "Running tests against python2.7\n"
+docker-compose run --rm test
 status=$?
 
-docker-compose stop -t0
+docker-compose down
+
+printf $status
+
+if [ "$status" != "0" ]; then exit $status; fi
+
+printf "\nRunning tests against python2.6"
+docker-compose run --rm test26
+status=$?
+
+docker-compose down
+
+printf $status
 
 exit $status
